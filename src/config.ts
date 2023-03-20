d1cc0db8a4e2d70b45588d4ff365f51b801ba7a9
Nhut Duong <hello@nhutduong.com>
2023-03-20 12:14:03 +0700

    ✨ Add SEO integration

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import type { Site } from "./types";
+
+export const SITE: Site = {
+  website: "https://nhutduong.com/",
+  author: "Nhut Duong",
+  title: "Nhut Duong",
+  desc: "A Vietnam-based full-stack web developer who specializes in JavaScript, AngularJS, React, Node.js, PHP and WordPress.",
+};
